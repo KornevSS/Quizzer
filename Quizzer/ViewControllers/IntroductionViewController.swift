32fa16c1682cd2778f94065c4da28e8e0725e7e8
S.Kornev <post.kornev@gmail.com>
2021-07-12 15:49:19 +0300

    Adding some logic

---
+++ Quizzer/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionViewController.swift
+//  Quizzer
+//
+//  Created by Сергей Корнев on 10.07.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
